95be24ec3aab90e25378013e54c6078b81664dc4
Ackin.Dev <ackinlino123@gmail.com>
2026-02-04 12:57:39 -0300

    feat: Migration for blog posts and migration with controller and model for development

---
+++ app/Models/Posts.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Posts extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'category',
+        'tags',
+    ];
+
+    protected $casts = [
+        'tags' => 'array',
+    ];
+
+    protected $attributes = [
+        'tags' => '[]'
+    ];
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/about', function (){
-    return response()->json(['message' => 'This is a RESTful Blogging Platform API.']);
-});
+Route::get('/posts', [PostsController::class, 'index']);
+Route::get('/posts/{id}', [PostsController::class, 'show']);
+Route::get('/posts?term=', [PostsController::class, 'search']);
+Route::post('/posts', [PostsController::class, 'store']);
+Route::put('/posts/{id}', [PostsController::class, 'update']);
+Route::patch('/posts/{id}', [PostsController::class, 'update']);
+Route::delete('/posts/{id}', [PostsController::class, 'destroy']);
